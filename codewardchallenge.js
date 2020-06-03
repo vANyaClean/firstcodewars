64e59e49508d59a005e2bc080b7ea5bac2cc3c49
vANyaClean <65251175+vANyaClean@users.noreply.github.com>
2020-06-03 18:17:34 +0300

    Update codewardchallenge.js

--- codewardchallenge.js
+++ codewardchallenge.js
@@ -1,36 +1,3 @@
-https://www.codewars.com/kata/57f781872e3d8ca2a000007e
-function maps(x){
-return x.map(n => n*2);
-
-}
-
-https://www.codewars.com/kata/5556282156230d0e5e000089
-function DNAtoRNA(dna) {
-return dna.replace(/T/g, "U");
-  // create a function which returns an RNA sequence from the given DNA sequence
-}
-
-https://www.codewars.com/kata/577a98a6ae28071780000989
-var min = function(list){  
-    return Math.min(...list);
-}
-
-var max = function(list){;
-    return Math.max(...list);
-}
-
-https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
-function stringToArray(string){
-let separ = " ";
-const splitarray = string.split(separ); 
- return splitarray;
- 
-  // code code code
-
-}
-решта не зробив. не розібрався бо не мав часу
-24 години в дорозі
-
 
 Mінімальне значення масиву
 class SmallestIntegerFinder {
