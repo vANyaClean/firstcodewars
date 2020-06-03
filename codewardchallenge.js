5d380164d6a406f369f27ee6fae76e142048b8bf
vANyaClean <65251175+vANyaClean@users.noreply.github.com>
2020-06-03 17:35:00 +0300

    Update codewardchallenge.js

--- codewardchallenge.js
+++ codewardchallenge.js
@@ -47,5 +47,34 @@ return circle.radius * 2 * Math.PI ;
 }
 
 
+Тварини
+
+class Shark extends Animal {
+  constructor(name, age,status) {
+    super(name , age, 0 , "shark",status);
+  }
+}
+
+class Cat extends Animal {
+constructor(name,age,status){
+ super(name , age ,4 ,"cat" , status)
+}
+introduce(){
+return  `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!` 
+}
+}
+class Dog extends Animal {
+  constructor(name ,age ,status ,master){
+  super(name,age,4,"dog",status , master)
+  this.master = master;}
+  intoroduce(){
+  return `Hello, my name is ${this.name} and I am ${this.age} years old.`
+  }
+  greetMaster(){
+  return `Hello ${this.master}`
+  }
+  
+}
+
 
 
